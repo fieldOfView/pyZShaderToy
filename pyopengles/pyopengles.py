bed6e2827d9c0c241bffc874f9d00a15afbcc320
Ben O'Steen <bosteen@gmail.com>
2012-05-11 07:32:47 +0100

    Refactored pyopengles.py, adding some convenience classes and moved the julia/mandlebrot demo to its own file (pyopengles_demo.py)

--- pyopengles/pyopengles.py
+++ pyopengles/pyopengles.py
@@ -16,7 +16,6 @@ import math
 from egl import *
 from gl2 import *
 from gl2ext import *
-import pymouse
 
 # Define verbose=True to get debug messages
 verbose = True
@@ -53,9 +52,36 @@ def check(e):
         print 'Error code',hex(e&0xffffffff)
     raise ValueError
 
+class ShaderCompilationFailed(Exception):
+    def __init__(self, reason=None):
+        self.reason = reason
+
+class GLError(Exception):
+    def __init__(self, code=None):
+        self.code = code
+
+    def __str__(self):
+        if self.code:
+            if self.code == GL_NO_ERROR:
+                return "No Error found (%s)" % self.code
+            elif self.code == GL_INVALID_ENUM:
+                return "A GLenum argument is out of range. The command that generated that error is ignored (%s)" % self.code
+            elif self.code == GL_INVALID_VALUE:
+                return "A numeric argument is out of range. The command that generated that error is ignored (%s)" % self.code
+            elif self.code == GL_INVALID_OPERATION:
+                return "The specific command cannot be performed in the current state. The command that generated this is ignored (%s)" % self.code
+            elif self.code == GL_OUT_OF_MEMORY:
+                return "There is insufficient memory to execute this command. The state of the OpenGL ES pipeline is considered to be undefined. All bets are off, basically. (%s)" % self.code
+            return "Unknown error code (%s)" % self.code
+        else:
+            return "No error code given"
+                
+
 class EGL(object):
 
-    def __init__(self, pref_width=None, pref_height=None):
+    def __init__(self, pref_width=None, pref_height=None, 
+    				   red_size=8, green_size=8,blue_size=8,
+    				   alpha_size=8, depth_size=None, other_attribs = []):
         """Opens up the OpenGL library and prepares a window for display"""
         b = bcm.bcm_host_init()
         assert b==0
@@ -63,13 +89,24 @@ class EGL(object):
         assert self.display
         r = openegl.eglInitialize(self.display,0,0)
         assert r
-        attribute_list = eglints(     (EGL_RED_SIZE, 8,
-                                      EGL_GREEN_SIZE, 8,
-                                      EGL_BLUE_SIZE, 8,
-                                      EGL_ALPHA_SIZE, 8,
-                                      EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
-                                      EGL_DEPTH_SIZE, 24,
-                                      EGL_NONE) )
+        
+        
+        attribute_list = [EGL_RED_SIZE, red_size,
+                          EGL_GREEN_SIZE, green_size,
+                          EGL_BLUE_SIZE, blue_size]
+        if alpha_size:
+            attribute_list.extend([EGL_ALPHA_SIZE, alpha_size])
+
+        attribute_list.extend([EGL_SURFACE_TYPE, EGL_WINDOW_BIT])
+        
+        if depth_size:
+            attribute_list.extend([EGL_DEPTH_SIZE, depth_size])
+        if other_attribs and len(other_attribs) % 2 == 0:
+            attribute_list.extend(other_attribs)
+        
+        attribute_list.append(EGL_NONE)
+        attribute_list = eglints( attribute_list )
+
         numconfig = eglint()
         config = ctypes.c_void_p()
         r = openegl.eglChooseConfig(self.display,
@@ -118,303 +155,115 @@ class EGL(object):
         r = openegl.eglMakeCurrent(self.display, self.surface, self.surface, self.context)
         assert r
 
-class demo():
+    def _check_Linked_status(self, programObject):
+        # Check the link status
+        linked = eglint()
+        opengles.glGetProgramiv ( programObject, GL_LINK_STATUS, ctypes.byref(linked))
+        try:
+            self._check_glerror()
+        except GLError, e:
+            print e
+            return False
 
-    def showlog(self,shader):
+        if (linked.value == 0):
+            print "Linking failed!"
+            loglength = eglint()
+            charswritten = eglint()
+            opengles.glGetProgramiv(programObject, GL_INFO_LOG_LENGTH, ctypes.byref(loglength))
+            logmsg = ctypes.c_char_p(" "*loglength.value)
+            opengles.glGetProgramInfoLog(programObject, loglength, ctypes.byref(charswritten), logmsg)
+            print logmsg.value
+            return False
+        return True
+    
+    def _check_glerror(self):
+        e=opengles.glGetError()
+        if e:
+            raise GLError(hex(e))
+        return
+
+    def _show_shader_log(self, shader):
         """Prints the compile log for a shader"""
         N=1024
         log=(ctypes.c_char*N)()
         loglen=ctypes.c_int()
         opengles.glGetShaderInfoLog(shader,N,ctypes.byref(loglen),ctypes.byref(log))
         print log.value
-
-    def showprogramlog(self,shader):
-        """Prints the compile log for a shader"""
+        
+    def _show_program_log(self, program):
+        """Prints the compile log for a program"""
         N=1024
         log=(ctypes.c_char*N)()
         loglen=ctypes.c_int()
-        opengles.glGetProgramInfoLog(shader,N,ctypes.byref(loglen),ctypes.byref(log))
+        opengles.glGetProgramInfoLog(program,N,ctypes.byref(loglen),ctypes.byref(log))
         print log.value
-            
-    def __init__(self):
-        self.vertex_data = eglfloats((-1.0,-1.0,1.0,1.0,
-                         1.0,-1.0,1.0,1.0,
-                         1.0,1.0,1.0,1.0,
-                         -1.0,1.0,1.0,1.0))
-        self.vshader_source = ctypes.c_char_p(
-              "attribute vec4 vertex;"
-              "varying vec2 tcoord;"
-              "void main(void) {"
-              "  vec4 pos = vertex;"
-              "  pos.xy*=0.9;"
-              "  gl_Position = pos;"
-              "  tcoord = vertex.xy*0.5+0.5;"
-              "}")
-      
-        self.fshader_source = ctypes.c_char_p(
-              "uniform vec4 color;"
-              "void main(void) {"
-              "   gl_FragColor = color;"
-              "}")
 
-        # Mandelbrot
-        mandelbrot_fshader_source = ctypes.c_char_p("""
-	uniform vec4 color;
-	uniform vec2 scale;
-	varying vec2 tcoord;
-	void main(void) {
-		float intensity;
-		vec4 color2;
-		float cr=(gl_FragCoord.x-810.0)*scale.x; //0.0005;
-		float ci=(gl_FragCoord.y-540.0)*scale.y; //0.0005;
+    def load_shader ( shader_src, shader_type = GL_VERTEX_SHADER, verbose = False ):
+        # Convert the src to the correct ctype, if not already done
+        if type(shader_src) == basestring:
+            shader_src = ctypes.c_char_p(shader_src)
 
-		float ar=cr;
-		float ai=ci;
-
-		float tr,ti;
-		float col=0.0;
-                float p=0.0;
-                int i=0;
-                
-                for(int i2=1;i2<16;i2++)
-                {
-                        tr=ar*ar-ai*ai+cr;
-                        ti=2.0*ar*ai+ci;
-                        p=tr*tr+ti*ti;
-                        ar=tr;
-                        ai=ti;
-                        if (p>16.0)
-                        {
-                          i=i2;
-                          break;
-                        }
-                        
-                }
-	        color2 = vec4(float(i)*0.0625,0,0,1);
-		gl_FragColor = color2;
-	}""")
-
-        # Julia
-        julia_fshader_source = ctypes.c_char_p("""
-	uniform vec4 color;
-	uniform vec2 scale;
-	uniform vec2 offset;
-	varying vec2 tcoord;
-	uniform sampler2D tex;
-	void main(void) {
-		float intensity;
-		vec4 color2;
-		float ar=(gl_FragCoord.x-810.0)*scale.x;
-		float ai=(gl_FragCoord.y-540.0)*scale.y;
-
-		float cr=(offset.x-810.0)*scale.x;
-		float ci=(offset.y-540.0)*scale.y;
-
-		float tr,ti;
-		float col=0.0;
-                float p=0.0;
-                int i=0;
-                int j=0;
-                vec2 t2;
-
-                t2.x=tcoord.x+(offset.x-810.0)*(1.0/1920.0);
-                t2.y=tcoord.y+(offset.y-540.0)*(1.0/1080.0);
-                
-                for(int i2=1;i2<16;i2++)
-                {
-                        tr=ar*ar-ai*ai+cr;
-                        ti=2.0*ar*ai+ci;
-                        p=tr*tr+ti*ti;
-                        ar=tr;
-                        ai=ti;
-                        if (p>16.0)
-                        {
-                          i=i2;
-                          break;
-                        }
-                }
-
-                col=float(j)*(0.005);
-	        color2 = vec4(col,float(i)*0.0625,0,1);
-	        color2 = color2+texture2D(tex,t2);
-		gl_FragColor = color2;
-	}""")
-
-        vshader = opengles.glCreateShader(GL_VERTEX_SHADER);
-        opengles.glShaderSource(vshader, 1, ctypes.byref(self.vshader_source), 0)
-        opengles.glCompileShader(vshader);
-
+        # Create a shader of the given type
         if verbose:
-            self.showlog(vshader)
-            
-        fshader = opengles.glCreateShader(GL_FRAGMENT_SHADER);
-        opengles.glShaderSource(fshader, 1, ctypes.byref(julia_fshader_source), 0);
-        opengles.glCompileShader(fshader);
+            print "Creating shader object"
+        shader = opengles.glCreateShader(shader_type)
+        opengles.glShaderSource(shader, 1, ctypes.byref(shader_src), 0)
+        opengles.glCompileShader(shader)
+  
+        compiled = eglint()
 
+        # Check compiled status
+        opengles.glGetShaderiv ( shader, GL_COMPILE_STATUS, ctypes.byref(compiled) )
+
+        if (compiled.value == 0):
+            print "Failed to compile shader '%s'" % shader_src 
+            loglength = eglint()
+            charswritten = eglint()
+            opengles.glGetShaderiv(shader, GL_INFO_LOG_LENGTH, ctypes.byref(loglength))
+            logmsg = ctypes.c_char_p(" "*loglength.value)
+            opengles.glGetShaderInfoLog(shader, loglength, ctypes.byref(charswritten), logmsg)
+            print logmsg.value
+            raise ShaderCompilationFailed(logmsg.value)
+        else:
+            shdtyp = "{unknown}"
+            if shader_type == GL_VERTEX_SHADER:
+                shdtyp = "GL_VERTEX_SHADER"
+            elif shader_type == GL_FRAGMENT_SHADER:
+                shdtyp = "GL_FRAGMENT_SHADER"
+            print "Compiled %s shader: %s" % (shdtyp, shader_src)
         if verbose:
-            self.showlog(fshader)
+            self._show_shader_log()
+        return shader
 
-        mshader = opengles.glCreateShader(GL_FRAGMENT_SHADER);
-        opengles.glShaderSource(mshader, 1, ctypes.byref(mandelbrot_fshader_source), 0);
-        opengles.glCompileShader(mshader);
+    def get_program(vertex_shader_src, fragment_shader_src, bindings=[], verbose=False):
+        # Load the vertex/fragment shaders (can throw a ShaderCompilationFailed exception)
+        vertexShader = LoadShader ( vertex_shader_src, GL_VERTEX_SHADER, verbose )
+        fragmentShader = LoadShader ( fragment_shader_src, GL_FRAGMENT_SHADER, verbose )
+        self._check_glerror()
 
+        # Create the program object
+        programObject = opengles.glCreateProgram ( )
+        self._check_glerror()
+
+        opengles.glAttachShader ( programObject, vertexShader )
+        opengles.glAttachShader ( programObject, fragmentShader )
+        self._check_glerror()
+
+        # Bind positions to attributes:
+        for pos, attrib in bindings:
+            opengles.glBindAttribLocation ( programObject, pos, attrib )
+        self._check_glerror()
+
+        # Link the program
+        opengles.glLinkProgram ( programObject )
+        self._check_glerror()
+
+        # Check the link status
+        if not (self._check_Linked_status(programObject)):
+            print "Couldn't link the shaders to the program object. Check the bindings and shader sourcefiles."
+            raise Exception
         if verbose:
-            self.showlog(mshader)
-
-        program = opengles.glCreateProgram();
-        opengles.glAttachShader(program, vshader);
-        opengles.glAttachShader(program, fshader);
-        opengles.glLinkProgram(program);
-
-        if verbose:
-            self.showprogramlog(program)
-            
-        self.program = program
-        self.unif_color = opengles.glGetUniformLocation(program, "color");
-        self.attr_vertex = opengles.glGetAttribLocation(program, "vertex");
-        self.unif_scale = opengles.glGetUniformLocation(program, "scale");
-        self.unif_offset = opengles.glGetUniformLocation(program, "offset");
-        self.unif_tex = opengles.glGetUniformLocation(program, "tex");
-        
-
-        program2 = opengles.glCreateProgram();
-        opengles.glAttachShader(program2, vshader);
-        opengles.glAttachShader(program2, mshader);
-        opengles.glLinkProgram(program2);
-
-        if verbose:
-            self.showprogramlog(program2)
-            
-        self.program2 = program2
-        self.attr_vertex2 = opengles.glGetAttribLocation(program2, "vertex");
-        self.unif_scale2 = opengles.glGetUniformLocation(program2, "scale");
-        self.unif_offset2 = opengles.glGetUniformLocation(program2, "offset");
-   
-        opengles.glClearColor ( eglfloat(0.0), eglfloat(1.0), eglfloat(1.0), eglfloat(1.0) );
-        
-        self.buf=eglint()
-        opengles.glGenBuffers(1,ctypes.byref(self.buf))
-
-        self.check()
-
-        # Prepare a texture image
-        self.tex=eglint()
-        self.check()
-        opengles.glGenTextures(1,ctypes.byref(self.tex))
-        self.check()
-        opengles.glBindTexture(GL_TEXTURE_2D,self.tex)
-        self.check()
-        # opengles.glActiveTexture(0)
-        #test_tex=(eglshort*(1920*1080))(*([3567]*20000))
-        #test_tex_p = ctypes.pointer(test_tex)
-        #self.store=[test_tex,test_tex_p]
-        opengles.glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,1920,1080,0,GL_RGB,GL_UNSIGNED_SHORT_5_6_5,0)
-        #opengles.glTexImage2D(GL_TEXTURE_2D,0,1920,1080,0,GL_RGB,GL_UNSIGNED_BYTE,0)
-        self.check()
-        opengles.glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, eglfloat(GL_NEAREST))
-        opengles.glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, eglfloat(GL_NEAREST))
-        self.check()
-        # Prepare a framebuffer for rendering
-        self.tex_fb=eglint()
-        opengles.glGenFramebuffers(1,ctypes.byref(self.tex_fb))
-        self.check()
-        opengles.glBindFramebuffer(GL_FRAMEBUFFER,self.tex_fb)
-        self.check()
-        opengles.glFramebufferTexture2D(GL_FRAMEBUFFER,GL_COLOR_ATTACHMENT0,GL_TEXTURE_2D,self.tex,0)
-        self.check()
-        opengles.glBindFramebuffer(GL_FRAMEBUFFER,0)
-        self.check()
-        # Prepare viewport
-        opengles.glViewport ( 0, 0, egl.width, egl.height );
-        self.check()
-        
-        # Upload vertex data to a buffer
-        opengles.glBindBuffer(GL_ARRAY_BUFFER, self.buf);
-        opengles.glBufferData(GL_ARRAY_BUFFER, ctypes.sizeof(self.vertex_data),
-                             ctypes.byref(self.vertex_data), GL_STATIC_DRAW);
-        opengles.glVertexAttribPointer(self.attr_vertex, 4, GL_FLOAT, 0, 16, 0);
-        opengles.glEnableVertexAttribArray(self.attr_vertex);
-        self.check()
-
-    def draw_mandelbrot_to_texture(self,scale):
-        # Draw the mandelbrot to a texture
-        opengles.glBindFramebuffer(GL_FRAMEBUFFER,self.tex_fb)
-        self.check()
-        opengles.glBindBuffer(GL_ARRAY_BUFFER, self.buf);
-        
-        opengles.glUseProgram ( self.program2 );
-        self.check()
-
-        opengles.glUniform2f(self.unif_scale2, eglfloat(scale), eglfloat(scale));
-        self.check()
-        #opengles.glUniform2f(self.unif_offset2, eglfloat(offset[0]), eglfloat(offset[1]));
-        #self.check()
-        opengles.glDrawArrays ( GL_TRIANGLE_FAN, 0, 4 );
-        self.check()
-               
-        opengles.glFlush()
-        opengles.glFinish()
-        self.check()
-        
-    def draw_triangles(self,scale=0.0005,offset=(0.2,0.3)):
-
-        # Now render to the main frame buffer
-        opengles.glBindFramebuffer(GL_FRAMEBUFFER,0)
-        # Clear the background (not really necessary I suppose)
-        opengles.glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-        self.check()
-        
-        opengles.glBindBuffer(GL_ARRAY_BUFFER, self.buf);
-        self.check()
-        opengles.glUseProgram ( self.program );
-        self.check()
-        opengles.glBindTexture(GL_TEXTURE_2D,self.tex)
-        self.check()
-        opengles.glUniform4f(self.unif_color, eglfloat(0.5), eglfloat(0.5), eglfloat(0.8), eglfloat(1.0));
-        self.check()
-        opengles.glUniform2f(self.unif_scale, eglfloat(scale), eglfloat(scale));
-        self.check()
-        opengles.glUniform2f(self.unif_offset, eglfloat(offset[0]), eglfloat(offset[1]));
-        self.check()
-        opengles.glUniform1i(self.unif_tex, 0); # I don't really understand this part, perhaps it relates to active texture?
-        self.check()
-        
-        opengles.glDrawArrays ( GL_TRIANGLE_FAN, 0, 4 );
-        self.check()
-
-        opengles.glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-        opengles.glFlush()
-        opengles.glFinish()
-        self.check()
-        
-        openegl.eglSwapBuffers(egl.display, egl.surface);
-        self.check()      
-        
-    def check(self):
-        e=opengles.glGetError()
-        if e:
-            print hex(e)
-            raise ValueError
-        
-def showerror():
-    e=opengles.glGetError()
-    print hex(e)
-    
-if __name__ == "__main__":
-    egl = EGL()
-    d = demo()
-    d.draw_mandelbrot_to_texture(0.003)
-    m=pymouse.start_mouse()
-    while 1:
-        #offset=(400,600)
-        offset=(m.x,m.y)
-        d.draw_triangles(0.003,offset)
-        time.sleep(0.01)
-        if m.finished:
-            break
-    showerror()
+            self._show_program_log(programObject)
+        return programObject
 
 
-        
-    
