578f3757ce769ba2c4bf0af498f3e48ddb7b1df2
fieldOfView <aldo@fieldofview.com>
2015-05-24 21:08:00 +0200

    Moved example out of python file

--- zshadertoy.py
+++ zshadertoy.py
@@ -2,27 +2,20 @@
 
 from pyopengles import *
 
-example_frag = """precision mediump float;
+vert_shader = """attribute vec3 position;
+
+void main() {
+	gl_Position = vec4(position, 1.);
+}"""
+
+
+empty_frag = """precision mediump float;
 uniform float time_ms;
 uniform vec2 mouse;
 uniform vec2 resolution;
 
-void main( void ) {
-	float mouse_charge = 10.;
-	vec3 charges[10];
-	charges[0] = vec3(0,0,10);
-	charges[1] = vec3(0.1,0.1,-10);
-	const int N = 2;	
-	vec2 position = gl_FragCoord.xy / resolution.xy - resolution.xy/2.0;
-	position.x = (gl_FragCoord.x - resolution.x/2.0)/resolution.x;
-	position.y = (gl_FragCoord.y - resolution.y/2.0)/resolution.x;
-	float r = distance(position.xy,vec2(mouse.x - 0.5, (mouse.y - 0.5)*resolution.y/resolution.x));
-	float s = mouse_charge/(r*r);
-	for(int i=0; i < N; i++){
-		r = distance(position.xy,charges[i].xy);
-		s += charges[i].z / (r*r);
-	}
-	gl_FragColor = vec4(fract(-log(s)*10.),fract(-log(s)*1.),fract(-log(s)/10.),1.);
+void main(void) {
+	gl_FragColor = vec4(1.,0.,0.,1.);
 }"""
 
 surface_tris = eglfloats( (  - 1.0, - 1.0, 1.0, 
@@ -86,13 +79,9 @@ def run_shader(frag_shader):
                 self.y = y
         mouse = FakeM(400,300)
 
-        _v_src = """attribute vec3 position;
-                     void main() {
-                            gl_Position = vec4( position, 1.0 );
-                     }"""
-
-        vertexShader = egl_inst.load_shader(_v_src, GL_VERTEX_SHADER )
+        vertexShader = egl_inst.load_shader(vert_shader, GL_VERTEX_SHADER )
         fragmentShader = egl_inst.load_shader(frag_shader, GL_FRAGMENT_SHADER)
+
         # Create the program object
         programObject = opengles.glCreateProgram ( )
 
@@ -160,7 +149,7 @@ def run_shader(frag_shader):
 if __name__ == "__main__":
     import sys
 
-    frag = example_frag
+    frag = empty_frag
     if len(sys.argv) == 2:
         glsl_file = open(sys.argv[1], "r")
         frag = glsl_file.read()
